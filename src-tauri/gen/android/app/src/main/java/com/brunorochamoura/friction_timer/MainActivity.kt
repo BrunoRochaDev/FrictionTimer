c5cc2f4715b742a0bf5e5d40d17836d16689f204
Bruno Rocha Moura <brunorochamoura@proton.me>
2026-06-30 13:18:58 +0100

    Disable edge-to-edge on Android

--- src-tauri/gen/android/app/src/main/java/com/brunorochamoura/friction_timer/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/brunorochamoura/friction_timer/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.brunorochamoura.friction_timer
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import androidx.core.view.WindowCompat
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
-    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
+    WindowCompat.setDecorFitsSystemWindows(window, true)
   }
 }
