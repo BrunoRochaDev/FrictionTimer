871d4cbfc18f4ce0c2cb1f82ab913aeace10c780
Bruno Rocha Moura <brunorochamoura@proton.me>
2026-06-30 13:55:13 +0100

    Apply Android system bar insets to app content

--- src-tauri/gen/android/app/src/main/java/com/brunorochamoura/friction_timer/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/com/brunorochamoura/friction_timer/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.brunorochamoura.friction_timer
 
 import android.os.Bundle
-import androidx.core.view.WindowCompat
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : TauriActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
     super.onCreate(savedInstanceState)
-    WindowCompat.setDecorFitsSystemWindows(window, true)
+
+    val content = findViewById<View>(android.R.id.content)
+    ViewCompat.setOnApplyWindowInsetsListener(content) { view, insets ->
+      val systemBars = insets.getInsets(
+        WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.displayCutout(),
+      )
+
+      view.updatePadding(
+        left = systemBars.left,
+        top = systemBars.top,
+        right = systemBars.right,
+        bottom = systemBars.bottom,
+      )
+
+      insets
+    }
+    ViewCompat.requestApplyInsets(content)
   }
 }
